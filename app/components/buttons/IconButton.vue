94a53fadb5edbb338a8dd78f1135655712d51851
zero86 <katanazero86@gmail.com>
2025-11-16 03:34:44 +0900

    - IconButton.vue 컴포넌트 작성

---
+++ app/components/buttons/IconButton.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import type { Component } from "vue";
+
+const props = withDefaults(
+    defineProps<{
+      icon: Component;
+      onClick: () => void
+      buttonClass?: string
+    }>(),
+    {
+      buttonClass: 'hover:bg-gray-200 rounded-sm p-2 cursor-pointer'
+    }
+)
+</script>
+
+<template>
+  <button :class="buttonClass" @click="props.onClick">
+    <component :is="props.icon" />
+  </button>
+</template>
+
+<style scoped>
+
+</style>
--- app/components/header/Header.vue
+++ app/components/header/Header.vue
@@ -1,9 +1,15 @@
 <script setup lang="ts">
-
+import MoonIcon from "~/components/icons/MoonIcon.vue";
+import IconButton from "~/components/buttons/IconButton.vue";
 </script>
 
 <template>
-
+<header class="h-16 border-b border-gray-200 px-8 py-4 flex items-center justify-between">
+  <p class="text-xl font-bold">Nuxt4 Weather</p>
+  <div>
+    <IconButton :icon="MoonIcon" :on-click="() => console.log('clicked')"/>
+  </div>
+</header>
 </template>
 
 <style scoped>
