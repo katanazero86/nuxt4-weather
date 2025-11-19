333868784a665a5606718270d63f929bb70238f4
zero86 <katanazero86@gmail.com>
2025-11-19 18:11:14 +0900

    - Dropdown.vue 컴포넌트 아이콘 추가 - 아이콘 click 이벤트 바인딩

--- app/components/dropdown/Dropdown.vue
+++ app/components/dropdown/Dropdown.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { useOutside } from "~/composable/useOutside";
+import ChvronDownIcon from "~/components/icons/ChvronDownIcon.vue";
 
 type Option = { label: string; value: string; }
 
@@ -31,16 +32,20 @@ const handleOptionClick = (targetOption: Option) => {
 
 <template>
   <div class="relative" ref="containerRef">
-    <input
-        class="bg-gray-50 border border-gray-300 cursor-pointer outline-none py-2 px-4 text-sm rounded-md dark:bg-slate-700 dark:border-gray-600"
-        :class="isOpen ? 'border-indigo-300 dark:border-indigo-500' : ''"
-        type="text"
-        readonly
-        :value="selectedOption ? selectedOption.label : ''"
-        :placeholder="placeholder"
-        @click="toggleDropdown"/>
+    <div class="relative">
+      <input
+          class="bg-gray-50 border border-gray-300 cursor-pointer outline-none py-2 px-4 text-sm rounded-md dark:bg-slate-700 dark:border-gray-600"
+          :class="isOpen ? 'border-indigo-300 dark:border-indigo-500' : ''"
+          type="text"
+          readonly
+          :value="selectedOption ? selectedOption.label : ''"
+          :placeholder="placeholder"
+          @click="toggleDropdown"/>
+      <ChvronDownIcon class="absolute right-4 top-1/2 transform -translate-y-1/2 cursor-pointer"
+                      @click="toggleDropdown"/>
+    </div>
     <ul v-if="isOpen"
-        class="absolute left-0 right-0 mt-2 py-1 text-sm border border-gray-300 rounded-sm bg-white max-h-[250px] overflow-y-auto z-10 dark:bg-slate-800 dark:border-gray-600">
+        class="absolute left-0 right-0 z-10 mt-2 py-1 text-sm border border-gray-300 rounded-sm bg-white max-h-[250px] overflow-y-auto dark:bg-slate-800 dark:border-gray-600">
       <li v-for="option in options" class="cursor-pointer py-1 px-4 hover:bg-gray-200 dark:hover:bg-slate-700"
           :class="selectedOption?.value === option.value ? 'bg-gray-200 dark:bg-slate-700': ''"
           @click="handleOptionClick(option)">
---
+++ app/components/icons/ChvronDownIcon.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+const props = withDefaults(
+    defineProps<{ iconClass?: string }>(),
+    {
+      iconClass: 'text-black dark:text-white'
+    }
+)
+</script>
+
+<template>
+  <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' width='18' height='18' :class="iconClass">
+    <path fill='none' stroke='currentColor' stroke-linecap='round' stroke-linejoin='round' stroke-width='2' d='m6 9l6 6l6-6'/>
+  </svg>
+</template>
+
+<style scoped>
+
+</style>
