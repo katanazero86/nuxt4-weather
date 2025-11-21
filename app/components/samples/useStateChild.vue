7747375d3dacf4dfbddca7d91a55d89ab752f2bb
zero86 <katanazero86@gmail.com>
2025-11-21 16:29:26 +0900

    - useState() 컴포저블 샘플 코드 작성

---
+++ app/components/samples/useStateChild.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { useCount } from "~/pages/use-state/useCount";
+
+const { increment } = useCount()
+
+const count = useState('count') // 이런식으로 바로 참조 가능
+</script>
+
+<template>
+  <h2>useStateChild: {{count}}</h2>
+  <button class="p-2 border border-indigo-600" @click="increment">increment</button>
+</template>
+
+<style scoped>
+
+</style>
---
+++ app/pages/use-state/index.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import UseStateChild from "~/components/samples/useStateChild.vue";
+import { useCount } from "~/pages/use-state/useCount";
+
+const { count } = useCount()
+
+</script>
+
+<template>
+  <h2>useState composable</h2>
+  <p>count : {{ count }}</p>
+  <UseStateChild />
+</template>
+
+<style scoped>
+
+</style>
